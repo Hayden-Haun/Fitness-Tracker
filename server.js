f7481f6e93dabf61e42e53de727b5e5b15bd1481
Hayden-Haun <haydenhaun@gmail.com>
2021-09-22 10:13:49 -0600

    adding changes at end of session

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const routes = require("./controllers");
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +12,8 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(routes);
 
 app.use(express.static("public"));
 

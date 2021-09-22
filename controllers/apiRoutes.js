8777e483579ac7afd02ac40a8fefa89960b8b349
Hayden-Haun <haydenhaun@gmail.com>
2021-09-22 16:13:22 -0600

    put route works, but will not populate exercises with correct information

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -5,7 +5,7 @@ const db = require("../models");
 //Create GET route to retrieve most recent workout
 router.get("/workouts", async (req, res) => {
   try {
-    const newestWorkout = await db.Workout.find({}).sort({ day: 1 });
+    const newestWorkout = await db.Workout.find({}).sort({ day: -1 });
     console.log(newestWorkout.exercises);
     return res.json(newestWorkout);
   } catch (error) {
@@ -15,8 +15,8 @@ router.get("/workouts", async (req, res) => {
 
 router.post("/workouts", async (req, res) => {
   try {
-    const newWorkout = await db.Workout.create(req.body);
-    return res.json(newWorkout);
+    const newWorkout = await db.Workout.create();
+    res.json(newWorkout);
   } catch (error) {
     console.log(error);
   }
@@ -37,7 +37,7 @@ router.put("/workouts/:id", async (req, res) => {
         new: true,
       }
     );
-    return res.json(updatedWorkout);
+    res.json(updatedWorkout);
   } catch (error) {
     console.log(error);
   }

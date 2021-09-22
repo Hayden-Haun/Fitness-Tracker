2d0512159c72445873da4a480cbcc70f65febfaf
Hayden-Haun <haydenhaun@gmail.com>
2021-09-22 11:24:24 -0600

    updated API Routes

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,3 +1,15 @@
-const router = require("express");
+const router = require("express").Router();
+
+const db = require("../models");
+
+router.get("/workouts", async (req, res) => {
+  try {
+    const newestWorkout = await db.Workout.findOne({}).sort({ day: -1 });
+    console.log(newestWorkout.exercises);
+    res.json(newestWorkout);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 module.exports = router;

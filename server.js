7501bae3fa0f72d28a6354d8e9ad229ece5e3e2c
Hayden-Haun <haydenhaun@gmail.com>
2021-09-22 21:31:07 -0600

    deploying heroku

--- server.js
+++ server.js
@@ -16,10 +16,15 @@ app.use(routes);
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
